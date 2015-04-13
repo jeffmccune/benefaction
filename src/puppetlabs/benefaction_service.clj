6b24872744e73162400b21f13647506a427ae935
Jeff McCune <jeff@puppetlabs.com>
2015-04-13 16:36:38 -0700

    Use webserver $PORT when defined in the environment
    Without this patch the application fails to run in Heroku because the
    application binds to the wrong TCP port.  Statically defining the TCP
    port in a configuration file is the root cause of this problem because
    the TCP port is dynamically allocated by Heroku on deployment of the
    application.  The port is passed to the application via the PORT
    environment variable.
    
    This patch addresses the problem by using the
    [override-webserver-settings!][override] API method to pass the value
    from the environment, if defined, to the WebserverService.
    
    [override]: https://github.com/puppetlabs/trapperkeeper-webserver-jetty9#override-webserver-settings

--- src/puppetlabs/benefaction_service.clj
+++ src/puppetlabs/benefaction_service.clj
@@ -6,11 +6,24 @@
 (defprotocol HelloService
   (hello [this caller]))
 
+(defn env-settings
+  "If the PORT environment variable is defined return a map with the key :port
+  bound to the Integer value of the environment variable.  Otherwise, return
+  nil. This function overrides the TCP port when running in a PaaS process
+  model, e.g. in Heroku."
+  []
+  (if-let [port-env (System/getenv "PORT")]
+    (let [port (Integer. port-env)
+          _ (log/info (format "Found PORT=%s environment variable" port))]
+      {:port port})))
+
 (trapperkeeper/defservice hello-service
   HelloService
-  []
+  [[:WebserverService override-webserver-settings!]]
   (init [this context]
     (log/info "Initializing hello service")
+    ; use $PORT if defined, e.g. in Heroku
+    (if-let [settings (env-settings)] (override-webserver-settings! settings))
     context)
   (start [this context]
     (log/info "Starting hello service")
